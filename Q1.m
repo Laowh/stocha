f31d1ba3d182b08e34b903c71aba5bb837b82898
Laowh <37332292+Laowh@users.noreply.github.com>
2018-03-13 12:39:33 +0100

    fini
    Q1a

--- Q1.m
+++ Q1.m
@@ -1,17 +1,21 @@
 alphabet = ['a' 'b' 'c' 'd']
 occurences = zeros(1,4);
+probability = zeros(1,4);
 Q = zeros(4,4);
 seq = 'adcacdadadadadcacdadcadbabadcadcadadcadcadcacadcdbadcadadadcdcdcadabadadbadadabadcdadadadbadcadcdbadcadbadadcadcadadcdcadadcdcadbabadcacadcadadadcadbadabadcadcadacadbabacadcadadadcdcadcdcdadcacadcdcda';
-
-%alphabet(1) = seq(1);
-%occurences(1) = occurences(1) + 1;
+pos = find (alphabet == seq(1),1);
+occurences(pos) = occurences(pos) + 1;
 
 for i=2 : length(seq)
   current_pos = find (alphabet == seq(i),1);
   occurences(current_pos) = occurences(current_pos) + 1;
   previous_pos = find (alphabet == seq(i-1),1);
-  Q(current_pos, previous_pos) = Q(current_pos, previous_pos);
+  Q(previous_pos,current_pos) = Q(previous_pos,current_pos) +1;
 end
 alphabet
-occurences
+pi0 = occurences/sum(occurences)
+for i=1 :4
+    Q(i,:) = Q(i,:)/ sum(Q(i,:));
+end
+
 Q
